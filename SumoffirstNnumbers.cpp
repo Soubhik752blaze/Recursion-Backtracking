30d9ca9597a7966df9b4806bfe17c28b050102cc
Soubhik Ghosh <soubhik285@gmail.com>
2022-06-07 01:41:15 +0530

    sum of first N numbers

--- SumoffirstNnumbers.cpp
+++ SumoffirstNnumbers.cpp
@@ -4,21 +4,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void sumNnumbers(int i,int sum)
+void sumNnumbers(int i,int sum,int n)
 {
-    if(i<1)
+    if(i>n)
         {
             cout<<sum;
             return ;
         }
-    sumNnumbers(i-1,sum+i);  
+    sumNnumbers(i+1,sum+i,n);  
 }
 int main()
 {
     int N;
     cout<<"Enter Value of N :- ";
     cin>>N;
-    sumNnumbers(N,0);
+    sumNnumbers(0,0,N);
     return 0;
 
 
