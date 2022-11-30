da8801aca2ab42cc701f592103e4b3e8ff8934f9
Soubhik <soubhik285@gmail.com>
2022-11-30 23:21:38 +0530

    code updated

--- printasubsequencewhosesumisk.cpp
+++ printasubsequencewhosesumisk.cpp
@@ -1,68 +1,49 @@
-// Print any 1 subsequences of a string whose sum is equal to K
-
-
-#include<bits/stdc++.h>
+// Question :- Print any 1 subsequences of an array whose sum is equal to K
+#include <bits/stdc++.h>
 using namespace std;
 int k;
 
-bool printsubsequence(int ind,int n, int arr[], vector<int> &res,int sum)
+bool printsubsequence(int ind, int n, int arr[], vector<int> &res, int sum, int k)
 {
-    if (ind==n)
+    if (ind == n)
+    {
+        if (sum == k)
         {
-            if(sum==k)
-            {
-                //condition satisfied
-                for (auto it: res)
-                    cout<<it<<" ";
-                cout<<endl; 
-                return true;
-            }
-            // condition not satisfied
-            return false;
+            // condition satisfied
+            for (auto it : res)
+                cout << it << " ";
+            cout << endl;
+            return true;
         }
-    
-    //take or pick the element to be in your subsequence
+        // condition not satisfied
+        return false;
+    }
+
+    // take or pick the element to be in your subsequence
     res.push_back(arr[ind]);
-    sum+=arr[ind];
-    if(printsubsequence(ind+1,n,arr,res,sum))
+    sum += arr[ind];
+    if (printsubsequence(ind + 1, n, arr, res, sum, k))
         return true;
+    //backtrack
     res.pop_back();
-    sum-=arr[ind];
+    sum -= arr[ind];
 
-    //Donot take or pick the element to be in your subsequence
-    if(printsubsequence(ind+1,n,arr,res,sum))
+    // Donot take or pick the element to be in your subsequence
+    if (printsubsequence(ind + 1, n, arr, res, sum, k))
         return true;
 
-    
     return false;
-    
 }
 int main()
 {
-    int n;
-    cout<<"Enter value of n :-";
-    cin>>n;
-    if(n<=0)
-       {
-            cout<<"wrong Input";
-            return 0;
-       }
-    cout<<"Enter the values :- ";
-    int arr[n];
-    for(int i=0;i<n;i++)
-        cin>>arr[i];
-    cout<<"Enter value of k :- ";
-    cin>>k;
-    if(k==0)
-        {
-            cout<<"{}";
-            return 0;
-        }
+    int arr[] = {1,2,3,4,5};
+    int n = 5;
     vector<int> res;
-    printsubsequence(0,n,arr,res,0)?cout<<"True":cout<<"False";
+    int target = 5;
+    printsubsequence(0, n, arr, res, 0, target) ? cout << "True" : cout << "False";
     return 0;
-    
 }
 
-    //Code by Blaze
-
+// Code by Blaze
+//TC -> O(2^n)
+//SC -> O(N) as ASS
