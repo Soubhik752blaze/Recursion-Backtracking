05e984d588c8318da503da952206324b9708fa0a
Soubhik <soubhik285@gmail.com>
2022-12-01 00:38:30 +0530

    Code updated

--- CountNoofSubsequenceswithsumk.cpp
+++ CountNoofSubsequenceswithsumk.cpp
@@ -1,56 +1,36 @@
-// Count all subsequences of a string whose sum is equal to K
+// Question :- Count all subsequences of a string whose sum is equal to K
 
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int k;
 
-int countsubsequence(int ind,int n, int arr[],int sum)
+int countsubsequence(int ind, int n, int arr[], int sum, int target)
 {
-    if (ind==n)
-        { 
-            if(sum==k)
-                return 1;
-            else 
-                return 0;
-        }
-    
-    //take or pick the element to be in your subsequence
-    sum+=arr[ind];
-    int l = countsubsequence(ind+1,n,arr,sum);
-    sum-=arr[ind];
+    if (ind == n)
+    {
+        if (sum == target)
+            return 1;
+        else
+            return 0;
+    }
 
-    //Donot take or pick the element to be in your subsequence
-    int r = countsubsequence(ind+1,n,arr,sum);
+    // take or pick the element to be in your subsequence
+    sum += arr[ind];
+    int l = countsubsequence(ind + 1, n, arr, sum, target);
+    sum -= arr[ind];
 
-    return l+r;                 /*starting from end cases, start adding up counts for both taken and non taken cases
-                                    and start cumulating with the help of recursion*/
-    
+    // Donot take or pick the element to be in your subsequence
+    int r = countsubsequence(ind + 1, n, arr, sum, target);
+
+    return l + r; /*starting from end cases, start adding up counts for both taken and non taken cases
+                      and start cumulating with the help of recursion*/
 }
 int main()
 {
-    int n;
-    cout<<"Enter value of n :-";
-    cin>>n;
-    if(n<=0)
-       {
-            cout<<"wrong Input";
-            return 0;
-       }
-    cout<<"Enter the values :- ";
-    int arr[n];
-    for(int i=0;i<n;i++)
-        cin>>arr[i];
-    cout<<"Enter value of k :- ";
-    cin>>k;
-    if(k==0)
-        {
-            cout<<"{}";
-            return 0;
-        }
-    cout<<"The count of such subsequences are :- "<<countsubsequence(0,n,arr,0);
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = 5;
+    int target = 5;
+    cout << "The count of such subsequences are :- " << countsubsequence(0, n, arr, 0, target);
     return 0;
-    
 }
 
-    //Code by Blaze
+// Code by Blaze
