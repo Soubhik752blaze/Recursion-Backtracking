06b36f8cfb7674a6cc23a8a96280cf4b8e966d41
Soubhik <soubhik285@gmail.com>
2022-12-01 00:36:34 +0530

    updated code

--- PrintSubsequenceswhosesumisk.cpp
+++ PrintSubsequenceswhosesumisk.cpp
@@ -1,63 +1,45 @@
-// Print all subsequences of a string whose sum is equal to K
+// Question :- Print all subsequences of a string whose sum is equal to K
 
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int k;
 void printit(vector<int> &res)
 {
-    for (auto it: res)
-        cout<<it<<" ";
-    cout<<endl;    
-    return; 
+    for (auto it : res)
+        cout << it << " ";
+    cout << endl;
+    return;
 }
-void printsubsequence(int ind,int n, int arr[], vector<int> &res,int sum)
+void printsubsequence(int ind, int n, int arr[], vector<int> &res, int sum, int k)
 {
-    if (ind==n)
-        {
-            if(sum==k)
-                printit(res);
-            return;
-        }
-    
-    //take or pick the element to be in your subsequence
-    res.push_back(arr[ind]);
-    sum+=arr[ind];
-    printsubsequence(ind+1,n,arr,res,sum);
-    res.pop_back();
-    sum-=arr[ind];
+    if (ind == n)
+    {
+        if (sum == k)
+            printit(res);
+        return;
+    }
 
-    //Donot take or pick the element to be in your subsequence
-    printsubsequence(ind+1,n,arr,res,sum);
-    
+    // take or pick the element to be in your subsequence
+    res.push_back(arr[ind]);
+    sum += arr[ind];
+    printsubsequence(ind + 1, n, arr, res, sum, k);
+    res.pop_back();
+    sum -= arr[ind];
+
+    // Donot take or pick the element to be in your subsequence
+    printsubsequence(ind + 1, n, arr, res, sum, k);
 }
 int main()
 {
-    int n;
-    cout<<"Enter value of n :-";
-    cin>>n;
-    if(n<=0)
-       {
-            cout<<"wrong Input";
-            return 0;
-       }
-    cout<<"Enter the values :- ";
-    int arr[n];
-    for(int i=0;i<n;i++)
-        cin>>arr[i];
-    cout<<"Enter value of k :- ";
-    cin>>k;
-    if(k==0)
-        {
-            cout<<"{}";
-            return 0;
-        }
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = 5;
     vector<int> res;
-    cout<<"The list of such subsequences are :- "<<endl;
-    printsubsequence(0,n,arr,res,0);
+    int target = 5;
+    cout << "The list of such subsequences are :- " << endl;
+    printsubsequence(0, n, arr, res, 0, target);
     return 0;
-    
 }
 
-    //Code by Blaze
-
+// Code by Blaze
+//TC -> O(2^n)
+//SC -> O(N) as ASS
