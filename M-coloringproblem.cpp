460b5fabbe806d22a927e1c6d8458a5d2efc1b6a
Soubhik <soubhik285@gmail.com>
2022-12-03 21:54:27 +0530

    code updated

--- M-coloringproblem.cpp
+++ M-coloringproblem.cpp
@@ -13,7 +13,7 @@ using namespace std;
 bool isSafe(int v, bool graph[V][V], int color[], int c)
 {
     for (int i = 0; i < V; i++)
-        if (graph[v][i] && c == color[i])
+        if ( v!= i && graph[v][i] && c == color[i])
             return false;
 
     return true;
