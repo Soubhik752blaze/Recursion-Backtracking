2d0e250ee7b5c31a779cc1ac496cffa146623bdd
Soubhik <soubhik285@gmail.com>
2022-12-05 02:23:35 +0530

    code updated for better readability

--- ratinmaze.cpp
+++ ratinmaze.cpp
@@ -1,81 +1,89 @@
-/*Rat in a maze problem. with directions to move as Down Up Right and Left.
-print all possible routes in lexological order*/
+/*Question :- Rat in a maze problem, Go from (0,0) to (n-1,n-1) with directions to move given as -> Down, Up, Right and Left.
+Print all possible routes in lexological order*/
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 class solution
-{   
-    void checkandprintpath(int i,int j,vector<vector<int>> &maze,vector<string> &ans, int n, string res, vector<vector<int>> &vis)
+{
+    void checkandprintpath(int i, int j, vector<vector<int>> &maze, vector<string> &ans, int n, string res, vector<vector<int>> &vis)
     {
-        if(i == n-1 && j == n-1 )
+        // base-case
+        if (i == n - 1 && j == n - 1)
         {
             ans.push_back(res);
             return;
         }
 
-        //Downward Movement
-        if(i+1<n && vis[i+1][j]==0 && maze[i+1][j]==1)
+        // general scenario
+        //  Downward Movement
+        if (i + 1 < n && vis[i + 1][j] == 0 && maze[i + 1][j] == 1)
         {
-            vis[i][j]=1;
-            checkandprintpath(i+1,j,maze,ans,n,res+'D',vis);
-            vis[i][j]=0;
+            vis[i][j] = 1;
+            checkandprintpath(i + 1, j, maze, ans, n, res + 'D', vis);
+            // back-track
+            vis[i][j] = 0;
         }
 
-        //Leftward Movement
-         if(j-1>=0 && vis[i][j-1]==0 && maze[i][j-1]==1)
+        // Leftward Movement
+        if (j - 1 >= 0 && vis[i][j - 1] == 0 && maze[i][j - 1] == 1)
         {
-            vis[i][j]=1;
-            checkandprintpath(i,j-1,maze,ans,n,res + 'L',vis);
-            vis[i][j]=0;
+            vis[i][j] = 1;
+            checkandprintpath(i, j - 1, maze, ans, n, res + 'L', vis);
+            // back-track
+            vis[i][j] = 0;
         }
 
-        //Rightward movement
-        if(j+1<n && vis[i][j+1]==0 && maze[i][j+1]==1)
+        // Rightward movement
+        if (j + 1 < n && vis[i][j + 1] == 0 && maze[i][j + 1] == 1)
         {
-            vis[i][j]=1;
-            checkandprintpath(i,j+1,maze,ans,n,res + 'R',vis);
-            vis[i][j]=0;
+            vis[i][j] = 1;
+            checkandprintpath(i, j + 1, maze, ans, n, res + 'R', vis);
+            // back-track
+            vis[i][j] = 0;
         }
 
-        //Upward movement
-        if(i-1>=0 && vis[i-1][j]==0 && maze[i-1][j]==1)
+        // Upward movement
+        if (i - 1 >= 0 && vis[i - 1][j] == 0 && maze[i - 1][j] == 1)
         {
-            vis[i][j]=1;
-            checkandprintpath(i-1,j,maze,ans,n,res + 'U',vis);
-            vis[i][j]=0;
+            vis[i][j] = 1;
+            checkandprintpath(i - 1, j, maze, ans, n, res + 'U', vis);
+            // back-track
+            vis[i][j] = 0;
         }
     }
 
 public:
-    vector<string> findpath(vector<vector<int>> &maze, int n )
+    vector<string> findpath(vector<vector<int>> &maze, int n)
     {
         vector<string> ans;
-        vector<vector<int>> vis;
-        for(int i=0;i<n;i++)
-            vis.push_back(vector<int> (n,0));
-        if(maze[0][0]==1)
-            checkandprintpath(0,0,maze,ans,n,"",vis);
+        // initialising visited vector to check which cells are visited
+        vector<vector<int>> vis(n, vector<int>(n, 0));
+        // just a check if starting position is unblocked
+        if (maze[0][0] == 1)
+            checkandprintpath(0, 0, maze, ans, n, "", vis);
         return ans;
-    }    
+    }
 };
 
 int main()
 {
-    vector < vector < int >> maze = {{1,0,0,0},{1,1,0,1},{1,1,0,0},{0,1,1,1}};
+    vector<vector<int>> maze = {{1, 0, 0, 0},
+                                {1, 1, 0, 1},
+                                {1, 1, 0, 0},
+                                {0, 1, 1, 1}};
     int n = 4;
     solution obj;
-    vector < string > result = obj.findpath(maze, n);
+    vector<string> result = obj.findpath(maze, n);
     if (result.size() == 0)
         cout << -1;
     else
-        for (int i = 0; i < result.size(); i++) 
+        for (int i = 0; i < result.size(); i++)
             cout << result[i] << " ";
-    cout << endl; 
+    cout << endl;
     return 0;
-    
 }
 
-    /*Code by Blaze
-     Time Complexity: O(4^(m*n)), because on every cell we need to try 4 different directions.
-     Space Complexity:  O(m*n) ,Maximum Depth of the recursion tree(auxiliary space).*/
+/*Code by Blaze
+ Time Complexity: O(4^(n*n)), because on every cell we need to try 4 different directions.
+ Space Complexity:  O(n*n) ,Maximum Depth of the recursion tree(auxiliary space).*/
