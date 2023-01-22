a4c20a9ea5e3c8ee58ec0ce99b72a9c043276dbd
Soubhik <soubhik285@gmail.com>
2023-01-22 17:10:41 +0530

    explanation modified

--- Striver SDE sheet/palindromepartition.cpp
+++ Striver SDE sheet/palindromepartition.cpp
@@ -1,73 +1,66 @@
-/* Question:- Given a string s, partition s such that every substring of the partition is a palindrome. 
+/* Question:- Given a string s, partition s such that every substring of the partition is a palindrome.
 Return all possible palindrome partitioning of s.*/
 
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution
+// function to check if a string is pallindrome
+bool isPalindrome(string s, int start, int end)
 {
-public:
-  vector<vector<string>> partition(string s)
-  {
-    vector<vector<string>> res;
-    vector<string> path;
-    func(0, s, path, res);
-    return res;
-  }
+	while (start <= end)
+	{
+		if (s[start++] != s[end--])
+			return false;
+	}
+	return true;
+}
 
-  void func(int index, string s, vector<string> &path,
-            vector<vector<string>> &res)
-  {
-    // base-case
-    if (index == s.size())
-    {
-      res.push_back(path);
-      return;
-    }
-    // general scenario -> check at every partition if pallindrome is possible or not
-    // if possible, put it into path vector and check for the rest of the string..
-    // if a solution, not found then back-track the steps
-    for (int i = index; i < s.size(); i++)
-    {
-      if (isPalindrome(s, index, i))
-      {
-        path.push_back(s.substr(index, i - index + 1));
-        func(i + 1, s, path, res);
-        path.pop_back();
-      }
-    }
-  }
-  // function to check if a string is pallindrome
-  bool isPalindrome(string s, int start, int end)
-  {
-    while (start <= end)
-    {
-      if (s[start++] != s[end--])
-        return false;
-    }
-    return true;
-  }
-};
+// Approach -> check at every partition if pallindrome is possible or not
+// if pallindrome is possible, put it into path vector and check for the rest of the string..
+// if a solution not found ultimately down the road, then back-track the steps and look for other possibilities.
+void func(int index, string s, vector<string> &path, vector<vector<string>> &res)
+{
+	// base-case -> if reached the end of string, means a proper pallindrome partition is done.
+	if (index == s.size())
+	{
+		res.push_back(path);
+		return;
+	}
+
+	// loop for all the indices and check if pallindrome is possible,
+	// if yes, take it in "path", and then look for same pallindrome partitions in the rest of the string(i+1)
+	for (int i = index; i < s.size(); i++)
+	{
+		// take if pallindrome possible
+		if (isPalindrome(s, index, i))
+		{
+			path.push_back(s.substr(index, i - index + 1));
+			func(i + 1, s, path, res);
+			path.pop_back(); // backtracking step
+		}
+
+		// not take option not there since, here we cant skip anything (not subsequences)
+	}
+}
+vector<vector<string>> palindromepartitioning(string s)
+{
+	vector<vector<string>> res;
+	vector<string> path;
+	func(0, s, path, res);
+	return res;
+}
+
 int main()
 {
-  string s = "aabb";
-  Solution obj;
-  vector<vector<string>> ans = obj.partition(s);
-  int n = ans.size();
-  cout << "The Palindromic partitions are :-" << endl;
-  cout << "[ ";
-  for (auto i : ans)
-  {
-    cout << "[ ";
-    for (auto substring : i)
-    {
-      cout << substring << " ";
-    }
-    cout << "] ";
-  }
-  cout << "]";
-
-  return 0;
+	string s = "aabb";
+	vector<vector<string>> ans = palindromepartitioning(s);
+	for (auto x : ans)
+	{	
+		cout << "[ ";
+		for (auto str : x)
+			cout << str << " ";
+		cout << "] "<<endl;
+	}
 }
 
 /*Time Complexity: O((2^n) * k * (n/2) )
